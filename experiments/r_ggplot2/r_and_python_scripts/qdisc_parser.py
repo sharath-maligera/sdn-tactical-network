285420f18c3b9ea02e8b0d36b12895bfa05c5316
sharath-maligera <sharath.maligera@gmail.com>
2021-01-26 15:51:14 +0100

    Updated with ets qdisc and without timeout experiment

--- experiments/r_ggplot2/r_and_python_scripts/qdisc_parser.py
+++ experiments/r_ggplot2/r_and_python_scripts/qdisc_parser.py
@@ -14,23 +14,23 @@ from pathlib import Path
 
 def view_qdisc_meter_log():
     experiments_folder = Path(__file__).resolve().parents[1]
-    qdisc_log = os.path.join(experiments_folder, os.path.join('data', os.path.join('with_shaping_and_ets_scheduling_no_timeout','qdisc_log_2_4_60_kbps.csv')))
+    qdisc_log = os.path.join(experiments_folder, os.path.join('data', os.path.join('with_shaping_and_ets_scheduling_no_timeout','qdisc_log_9_6_240_kbps.csv')))
     df_qdisc = pd.read_csv(qdisc_log)
-    meter_log = os.path.join(experiments_folder, os.path.join('data',os.path.join('with_shaping_and_ets_scheduling_no_timeout','meter_log_2_4_60_kbps.csv')))
+    meter_log = os.path.join(experiments_folder, os.path.join('data',os.path.join('with_shaping_and_ets_scheduling_no_timeout','meter_log_9_6_240_kbps.csv')))
     df_meter = pd.read_csv(meter_log)
     pass
 
 def parse_qdisc_meter_log():
     experiments_folder = Path(__file__).resolve().parents[1]
-    qdisc_log = os.path.join(experiments_folder, os.path.join('data', os.path.join('with_shaping_and_ets_scheduling_no_timeout', 'qdisc_log_0_6_15_kbps.csv')))
-    df_qdisc = pd.read_csv(qdisc_log)
-    start_time = df_qdisc["timestamp"].min()
-    format = '%Y-%m-%d %H:%M:%S.%f'
-    duration_timestamp = [datetime.datetime.strptime(t1, format) - datetime.datetime.strptime(start_time, format) for t1, t2 in zip(df_qdisc["timestamp"], df_qdisc["timestamp"]) if not (t1!=t1 or t2!=t2)]
-    duration_in_secs = [duration.total_seconds() for duration in duration_timestamp]
-    df_qdisc.insert(6, "duration_in_secs", duration_in_secs, True)
-    df_qdisc = pd.DataFrame(df_qdisc, columns=['duration_in_secs', 'handle', 'packets', 'bytes'])
-    df_qdisc.to_csv(os.path.join(os.path.dirname(__file__), 'plot_qdisc_log_0_6_15_kbps.csv'), index=False, header=True)
+    # qdisc_log = os.path.join(experiments_folder, os.path.join('data', os.path.join('with_shaping_and_ets_scheduling_no_timeout', 'qdisc_log_9_6_240_kbps.csv')))
+    # df_qdisc = pd.read_csv(qdisc_log)
+    # start_time = df_qdisc["timestamp"].min()
+    # format = '%Y-%m-%d %H:%M:%S.%f'
+    # duration_timestamp = [datetime.datetime.strptime(t1, format) - datetime.datetime.strptime(start_time, format) for t1, t2 in zip(df_qdisc["timestamp"], df_qdisc["timestamp"]) if not (t1!=t1 or t2!=t2)]
+    # duration_in_secs = [duration.total_seconds() for duration in duration_timestamp]
+    # df_qdisc.insert(6, "duration_in_secs", duration_in_secs, True)
+    # df_qdisc = pd.DataFrame(df_qdisc, columns=['duration_in_secs', 'handle', 'packets', 'bytes'])
+    # df_qdisc.to_csv(os.path.join(os.path.dirname(__file__), 'plot_qdisc_log_9_6_240_kbps.csv'), index=False, header=True)
 
     meter_log = os.path.join(experiments_folder, os.path.join('data', os.path.join('with_shaping_and_ets_scheduling_no_timeout', 'meter_log_0_6_15_kbps.csv')))
     df_meter = pd.read_csv(meter_log)
@@ -40,7 +40,7 @@ def parse_qdisc_meter_log():
                           t1, t2 in zip(df_meter["timestamp"], df_meter["timestamp"]) if not (t1 != t1 or t2 != t2)]
     duration_in_secs = [duration.total_seconds() for duration in duration_timestamp]
     df_meter.insert(4, "duration_in_secs", duration_in_secs, True)
-    df_meter = pd.DataFrame(df_meter, columns=['duration_in_secs', 'handle', 'packets', 'bytes'])
+    df_meter = pd.DataFrame(df_meter, columns=['duration_in_secs', 'meter_id', 'packets', 'bytes'])
     df_meter.to_csv(os.path.join(os.path.dirname(__file__), 'plot_meter_log_0_6_15_kbps.csv'), index=False,header=True)
 
 
